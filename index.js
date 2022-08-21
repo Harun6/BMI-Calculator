e63f2bd8572aa90bd6b6c75ceee9c2a3809c7721
Harun Akgul <46harunakgul@gmail.com>
2022-08-21 15:33:14 +0300

    Create HTML an js files.

---
+++ index.js
@@ -0,0 +1,24 @@
+function bmıCalculator() {
+    var height = document.getElementById("height").value;
+    var weight = document.getElementById("weight").value;
+
+    height = height / 100;
+    var bmıcalculate = weight / (height*height);
+
+    var status = "";
+
+    if (bmıcalculate < 18.5) {
+        status = "Underweight";    
+    }
+    else if (bmıcalculate >= 18.5 && bmıcalculate < 24.9) {
+        status = "Healty";
+    }
+    else if (bmıcalculate >= 24.9 && bmıcalculate < 30){ 
+        status = "Overweight";
+    }
+    else{
+        status = "Obese";
+    }
+
+    document.querySelector('.comment').innerHTML = `Comment you are <span id="comment"> ${status}</span>`;
+}
